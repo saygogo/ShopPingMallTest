c8a5ed3cbcc01e7690723fa6ce8e370a61119aaf
saygogo <1183769411@qq.com>
2017-06-15 11:37:41 +0800

    实现Home页的头布局

--- app/src/main/java/com/example/dontworry/shoppingmall3/home/fragment/HomeFragment.java
+++ app/src/main/java/com/example/dontworry/shoppingmall3/home/fragment/HomeFragment.java
@@ -1,13 +1,21 @@
 package com.example.dontworry.shoppingmall3.home.fragment;
 
-import android.graphics.Color;
-import android.util.Log;
-import android.view.Gravity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.example.dontworry.shoppingmall3.R;
 import com.example.dontworry.shoppingmall3.base.BaseFragment;
 import com.example.dontworry.shoppingmall3.type.fragment.TypeFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * Created by Don't worry on 2017/6/15.
@@ -16,22 +24,43 @@ import com.example.dontworry.shoppingmall3.type.fragment.TypeFragment;
 public class HomeFragment extends BaseFragment {
 
     private static final String TAG = TypeFragment.class.getSimpleName();
-    private TextView textView;
+    @BindView(R.id.tv_search_home)
+    TextView tvSearchHome;
+    @BindView(R.id.tv_message_home)
+    TextView tvMessageHome;
+    @BindView(R.id.rv_home)
+    RecyclerView rvHome;
+    @BindView(R.id.ib_top)
+    ImageButton ibTop;
+    Unbinder unbinder;
 
     @Override
     public View initView() {
-        Log.e(TAG,"初始化分类控件...");
-        textView = new TextView(context);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(25);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View view = View.inflate(context, R.layout.fragment_home, null);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        Log.e(TAG,"绑定数据到控件上...");
-        textView.setText("我是主页内容");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick({R.id.tv_search_home, R.id.tv_message_home, R.id.ib_top})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_search_home:
+                break;
+            case R.id.tv_message_home:
+                break;
+            case R.id.ib_top:
+                break;
+        }
     }
 }
